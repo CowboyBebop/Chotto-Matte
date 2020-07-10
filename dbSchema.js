f6d0578f94b224a816fa3f7ccb6509a49139efd5
RebornInferno <antonpirogov@mail.ru>
2020-07-10 18:30:57 +0300

    changed to express, added create function

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    tuturus: [
+        {
+            userHandle: 'user',
+            body: "tuturu body",
+            createdAt: "2020-07-10T11:08:07.779Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -4,36 +4,41 @@ const { ResultStorage } = require("firebase-functions/lib/providers/testLab");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+const express = require('express');
+const app = express();
 
-exports.getTuturu = functions.https.onRequest((req, res) => {
+
+
+app.get('/alltuturu',(req, res) => {
   admin
     .firestore()
     .collection('tuturu')
+    .orderBy('createdAt','desc')
     .get()
     .then((data) => {
       let tuturus = [];
       data.forEach((doc) => {
-        tuturus.push(doc.data());
+        tuturus.push({
+          tuturuId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(tuturus);
     })
     .catch((err) => console.error(err));
 });
 
-exports.createTuturu = functions.https.onRequest((req, res) => {
+app.post('/createTuturu', (req, res) => {
+
   const newTuturu = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
-  admin.firestore()
+  admin.firestore() 
     .collection('tuturu')
     .add(newTuturu)
     .then(doc => {
@@ -44,3 +49,5 @@ exports.createTuturu = functions.https.onRequest((req, res) => {
       console.error(err);
     })
 });
+
+exports.api = functions.region('europe-west3').https.onRequest(app);
